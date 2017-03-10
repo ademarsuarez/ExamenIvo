579ca4edb97990897d8a17600a20664a4d1f59c8
ademarsuarez <ademarsuarez19@gmail.com>
2017-03-10 12:04:43 -0600

    EXAMEN
    EXAMEN

---
+++ examen/funciones.js
@@ -0,0 +1,21 @@
+var bacoB = angular.module("bacoB,[]")
+bacoB.controller ("mensaje",function($scope){
+
+	$scope.cantidad = 1;
+	$scope.cantidad2 = 2;
+	$scope.total = 0;
+
+	$scope.total = $scope.cantidad1 = $scope.cantidad2
+});
+	
+
+	operaciones = angular.module("operaciones", []);
+	operaciones.controller("suma" , function()
+});
+   bacoB.controller ("mensaje",function($scope){
+   	$scope.cantidad = 1;
+	$scope.cantidad2 = 2;
+	$scope.total = 0;
+ 
+ $scope.total = $scope.cantidad1 = $scope.cantidad2
+});
